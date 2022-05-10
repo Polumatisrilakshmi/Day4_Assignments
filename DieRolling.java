9047b1835b88d7f4822122de75abdd1f8c8322c8
Polumatisrilakshmi <polumatisrilakshmi@gmail.com>
2022-05-10 18:48:56 +0530

    Added:Added Die Rolling

---
+++ DieRolling.java
@@ -0,0 +1,19 @@
+package COM.BRIDGELABZ;
+
+public class DieRolling {
+    static final int START_POSITION = 0;
+
+        public static void main(String[] args) {
+            // welcome to snake ladder program.
+            System.out.println("Welcome to snake ladder game");
+            System.out.println("Player will start from zero position.");
+
+            int dieRolling = (int) Math.floor(Math.random() * 10) % 7;
+            if (dieRolling == 0) {
+                dieRolling = dieRolling + 1;
+
+            }
+            System.out.println("Number on die is: " + dieRolling);
+        }
+    }
+
